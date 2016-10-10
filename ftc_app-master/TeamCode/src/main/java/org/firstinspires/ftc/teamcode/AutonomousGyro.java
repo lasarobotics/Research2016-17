0c31408c34142bc96fe775c23eeca73e6da5bcdd
Ethan Schaffer <etanbet@gmail.com>
2016-10-10 14:41:07 -0600

    Revert "Revert "Addedd Sensor Code""
    This reverts commit bda19fdd9f552dad1855abd741225b9a56a6c8b7.

---
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousGyro.java
@@ -0,0 +1,31 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cGyro;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.robocol.TelemetryMessage;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+/**
+ * Created by Ethan Schaffer on 9/24/2016.
+ */
+@Autonomous(name = "GyroAutoTurn", group = "Autonomous")
+@Disabled   // comment out or remove this line to enable this opmode
+public class AutonomousGyro extends LinearOpMode {
+    ModernRoboticsI2cGyro gyroSensor;
+    @Override
+    public void runOpMode() throws InterruptedException {
+        gyroSensor = hardwareMap.get(ModernRoboticsI2cGyro.class, "g");
+
+        waitForStart();
+
+        while(opModeIsActive()){
+            telemetry.addData("Gyro Reading", gyroSensor.getHeading());
+            telemetry.update();
+            idle();
+        }
+    }
+}
