3be5186e6245754ff516ac756b82b66fec7ac50b
Ethan Schaffer <etanbet@gmail.com>
2016-11-02 12:11:26 -0600

    General Tweaks
    Added Infeed Enum, Slowing Button, Comments
    
    The Ascii will make it easier to find certain sections, I promise!

--- ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpFinal.java
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpFinal.java
@@ -1,11 +1,9 @@
 package org.firstinspires.ftc.teamcode;
-
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
-
 import java.util.Arrays;
 
 /**
@@ -13,25 +11,35 @@ import java.util.Arrays;
  */
 @TeleOp(name="Driver Controlled", group="TeleOp")
 public class TeleOpFinal extends OpMode {
-    public static final double THRESHOLD = .1;
+
+    //TWEAKING VALUES
     public static final double BLOCKSERVOOPENVALUE = 1;
     public static final double BLOCKSERVOCLOSEDVALUE = 0;
-    public static final double MAXINFEEDPOWER = .5;
+    public static final double MAXINFEEDPOWER = 1;
     public static final double MAXOUTFEEDPOWER = -1;
-    public static final double MININFEEDPOWER = 0;
+    public static final double INFEEDOFFPOWER = 0;
     public static final double SERVOINCREMENTVALUE = .02;
     public static final double LEFTSERVOMAXVALUE = 1;
     public static final double LEFTSERVOMINVALUE = 0;
     public static final double RIGHTSERVOMAXVALUE = 1;
     public static final double RIGHTSERVOMINVALUE = 0;
 
+    //GOOD VALUES
+    public enum GOING {
+        IN, OUT, NOTGOING
+    }
+    public GOING INFEEDSTATUS = GOING.NOTGOING;
+    public static final double SLOWDOWNVALUE = 5;
+    public static final double TRIGGERTHRESHOLD = .2;
+    public static final double ACCEPTINPUTTHRESHOLD = .15;
     public static final double SHOOTERMAXVALUE = 1;
     public static final double SHOOTERMINVALUE = 0;
     public static final double SHOOTERTHRESHOLD = .2;
     public static final double SHOOTERLOWERRATE = 0;
-
+    //I know these should be switched, but the hardware map is a pain to fix.
     public static final String LEFT1NAME = "l2";
     public static final String LEFT2NAME = "l1";
+    //I know these should be switched, but the hardware map is a pain to fix.
     public static final String RIGHT1NAME = "r1";
     public static final String RIGHT2NAME = "r2";
     public static final String SHOOT1NAME = "sh1";
@@ -44,7 +52,6 @@ public class TeleOpFinal extends OpMode {
 
     DcMotor left1, left2, right1, right2, shoot1, shoot2, infeed;
     Servo leftPush, rightPush, ballBlock;
-    boolean infeedOn = false;
     @Override
     public void init() {
         left1 =hardwareMap.dcMotor.get(LEFT1NAME);
@@ -67,46 +74,103 @@ public class TeleOpFinal extends OpMode {
 
     @Override
     public void loop() {
-        if(gamepad2.left_bumper) {
-            infeed.setPower(MAXINFEEDPOWER);
-        } else if(gamepad2.right_bumper) {
-            infeed.setPower(MAXOUTFEEDPOWER);
-        } else {
-            infeed.setPower(MININFEEDPOWER);
-        }
-        arcadeMecanum(Math.abs(gamepad1.left_stick_y) > THRESHOLD ? -gamepad1.left_stick_y : 0,
-                    Math.abs(gamepad1.left_stick_x) > THRESHOLD ? gamepad1.left_stick_x : 0,
-                    Math.abs(gamepad1.right_stick_x)> THRESHOLD ? -gamepad1.right_stick_x: 0,
-                    left1, right1, left2, right2);
-        //The above code uses Ternary Operations to save space. The arcade() method can be found below.
 
-/*        left1.setPower(0);
-        left2.setPower(0);
-        right1.setPower(0);
-        right2.setPower(0);
-
-        if(gamepad1.a)
-            left1.setPower(1); //l2
-        if(gamepad1.b)
-            left2.setPower(1);
-        if(gamepad1.x)
-            right1.setPower(1);
-        if(gamepad1.y)
-            right2.setPower(1);
+/*
+  _        __              _
+ (_)      / _|            | |
+  _ _ __ | |_ ___  ___  __| |
+ | | '_ \|  _/ _ \/ _ \/ _` |
+ | | | | | ||  __/  __/ (_| |
+ |_|_| |_|_| \___|\___|\__,_|
 */
+        if(gamepad2.left_bumper){
+            INFEEDSTATUS =(INFEEDSTATUS == GOING.IN) ? GOING.NOTGOING : GOING.IN;
+        } else if (gamepad2.right_bumper){
+            INFEEDSTATUS =(INFEEDSTATUS == GOING.OUT) ? GOING.NOTGOING : GOING.OUT;
+        }
+        //Ternary Operations used to control the state
+        switch(INFEEDSTATUS){
+            case IN:
+                infeed.setPower(MAXINFEEDPOWER);
+                break;
+            case OUT:
+                infeed.setPower(MAXOUTFEEDPOWER);
+                break;
+            case NOTGOING:
+                infeed.setPower(INFEEDOFFPOWER);
+            default:
+                infeed.setPower(INFEEDOFFPOWER);
+        }
+
+/*
+      _      _
+     | |    (_)
+   __| |_ __ ___   _____
+  / _` | '__| \ \ / / _ \
+ | (_| | |  | |\ V /  __/
+  \__,_|_|  |_| \_/ \___|
+ */
+        double inputX = Math.abs(gamepad1.left_stick_y) > ACCEPTINPUTTHRESHOLD ? -gamepad1.left_stick_y : 0;
+        double inputY = Math.abs(gamepad1.left_stick_x) > ACCEPTINPUTTHRESHOLD ? gamepad1.left_stick_x : 0;
+        double inputC = Math.abs(gamepad1.right_stick_x)> ACCEPTINPUTTHRESHOLD ? -gamepad1.right_stick_x: 0;
+
+        double BIGGERTRIGGER = gamepad1.left_trigger > gamepad1.right_trigger ? gamepad1.left_trigger : gamepad1.right_trigger;
+        //Ternary, the larger trigger value is set to the value BIGGERTRIGGER
+
+        if(BIGGERTRIGGER > TRIGGERTHRESHOLD){
+                inputX /= SLOWDOWNVALUE*BIGGERTRIGGER;
+                inputY /= SLOWDOWNVALUE*BIGGERTRIGGER;
+                inputC /= SLOWDOWNVALUE*BIGGERTRIGGER;
+        }
+        //Use the larger trigger value to scale down the inputs.
+        arcadeMecanum(inputX, inputY, inputC, left1, right1, left2, right2);
+
+/*
+  ___  ___ _ ____   _____
+ / __|/ _ \ '__\ \ / / _ \
+ \__ \  __/ |   \ V / (_) |
+ |___/\___|_|    \_/ \___/
+ */
+
         setServo(leftPush, gamepad2.a, gamepad2.b, SERVOINCREMENTVALUE, LEFTSERVOMAXVALUE, LEFTSERVOMINVALUE);
         setServo(rightPush, gamepad2.x, gamepad2.y, SERVOINCREMENTVALUE, RIGHTSERVOMAXVALUE, RIGHTSERVOMINVALUE);
-        telemetry.addData("left", leftPush.getPosition());
-        telemetry.addData("left", leftPush.getPosition());
-        telemetry.update();
-        //Servos can be set to go between 1 and 0, and will be incremented by 2% of their range each cycle.
-        //Pressing the 'a', 'b','x', or 'y' button will increment the servo.
 
+/*
+      _                 _
+     | |               | |
+  ___| |__   ___   ___ | |_
+ / __| '_ \ / _ \ / _ \| __|
+ \__ \ | | | (_) | (_) | |_
+ |___/_| |_|\___/ \___/ \__|
+*/
         shoot((gamepad1.right_bumper || gamepad1.left_bumper), ballBlock, shoot1, shoot2,
                 BLOCKSERVOOPENVALUE, BLOCKSERVOCLOSEDVALUE,
                 SHOOTERLOWERRATE, SHOOTERMAXVALUE, SHOOTERMINVALUE, SHOOTERTHRESHOLD);
         //The shooter will shoot at full power, will slow down by diving it's power by 1.5, and will stop completely once it's power is lower than .2.
         //This will hopefully help us to avoid breakage of the gearbox.
+
+
+/*
+  _       _                     _
+ | |     | |                   | |
+ | |_ ___| | ___ _ __ ___   ___| |_ _ __ _   _
+ | __/ _ \ |/ _ \ '_ ` _ \ / _ \ __| '__| | | |
+ | ||  __/ |  __/ | | | | |  __/ |_| |  | |_| |
+  \__\___|_|\___|_| |_| |_|\___|\__|_|   \__, |
+                                          __/ |
+                                         |___/
+*/
+        telemetry.addData("leftServo", leftPush.getPosition());
+        telemetry.addData("rightServo", rightPush.getPosition());
+        telemetry.addData("Controller LeftY", gamepad1.left_stick_y);
+        telemetry.addData("Controller LeftX", gamepad1.left_stick_x);
+        telemetry.addData("Controller RightY", gamepad1.right_stick_y);
+        telemetry.addData("Infeed", INFEEDSTATUS == GOING.IN ? "Forwards" : INFEEDSTATUS == GOING.OUT ? "Backwards" : "None");
+        //Ternary, basically it just outputs the Infeed state.
+        telemetry.update();
+        //Servos can be set to go between 1 and 0, and will be incremented by 2% of their range each cycle.
+        //Pressing the 'a', 'b','x', or 'y' button will increment the servo.
+
 
     }
 
