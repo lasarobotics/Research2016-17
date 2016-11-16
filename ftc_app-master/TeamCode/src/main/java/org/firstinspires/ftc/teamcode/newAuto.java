01375d28548bfe06f2d08ce6b49ac8d79e3cdd6f
Ethan Schaffer <etanbet@gmail.com>
2016-11-15 22:39:49 -0600

    Uh oh
    unchecked a box, this file should be part of the last commit

--- ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/newAuto.java
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/newAuto.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cGyro;
 import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cRangeSensor;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -44,13 +45,25 @@ public class newAuto extends LinearOpMode {
 
     public static final double POWER1 = -.5, DISTANCE1 = 1900, DISTANCE1PART2 = 1000;           //Distance Forwards before shot
 
-    public static final long SHOTTIMEINMILLISECONDS = 4500;              //How long we shoot the ball
+    public static final long SHOTTIMEINMILLISECONDS = 1000;              //How long we shoot the ball
+//    public static final long SHOTTIMEINMILLISECONDS = 4500;              //How long we shoot the ball
+//UPDATE THIS
+
+
+    //TODO: Make the numbers actually count out our steps
+    //Don't do this until it all works, and then do it as a refactor
+    //Once the refactor is done, make a pretty 'map'
     public static final double POWER2 = .2, DISTANCE2 = 500;             //Backup Distance
-    public static final double POWER3 = .5, DISTANCE3 = 1500;            //Strafe Distance
-    public static final double POWER4 = -.35, DISTANCE4 = 2500;           //Distance Forwards to get to strafing position
-    public static final double POWER5 = .45, CM_FROM_WALL_VALUE = 12;     //Strafe to between the white lines.
-    public static final double POWER6 = -.2, COLOR_READING_FOR_LINE = 4; //Forwards to Beacon #2
-    public static final double POWER7 = .2;                              //Backwards to beacon #1
+    public static final double POWER2PART2 = .25, GYRO2READINGTARGET = 37; //Turn Reading
+    public static final double POWER3 = -.5, DISTANCE3 = 2750;            //Strafe Distance
+    public static final double POWER3PART2 = -.25, GYRO3READINGTARGET = 0; //Turn Reading
+    public static final double POWER4 = -.35, DISTANCE4 = 2500;          //Distance Forwards to get to strafing position
+    public static final double POWER5 = .45, CM_FROM_WALL_VALUE = 8;    //Strafe to between the white lines.
+    public static final double POWER6 = .2, COLOR_READING_FOR_LINE = 4; //Backwards to beacon #1
+    public static final double POWER7 = -.2; //Forwards to Beacon #2
+    public  static  final double POWER7PART2 = .2, GYRO7READINGTARGET = 45;
+    public static final double POWER8 = .5, DISTANCE8 = 2500; //Distance to Ball
+
 
     DcMotor leftFrontWheel, leftBackWheel, rightFrontWheel, rightBackWheel, shoot1, shoot2, infeed;
     Servo leftButtonPusher, rightButtonPusher, ballBlock;
@@ -115,14 +128,16 @@ public class newAuto extends LinearOpMode {
         shoot2.setPower(0);
         ballBlock.setPosition(BALLBLOCKCLOSED);
 
+/*
         resetEncoder(leftFrontWheel);
         sleep(50);
         while(Math.abs(leftFrontWheel.getCurrentPosition()) < DISTANCE1PART2) {
             arcade(POWER1, 0, 0, leftFrontWheel, rightFrontWheel, leftBackWheel, rightBackWheel);
         }
         stopMotors();
-/*
+*/
 
+        //Back Up
         resetEncoder(leftFrontWheel);
         sleep(50);
         while(Math.abs(leftFrontWheel.getCurrentPosition()) < DISTANCE2) {
@@ -130,13 +145,38 @@ public class newAuto extends LinearOpMode {
         }
         stopMotors();
 
-        resetEncoder(leftFrontWheel);
-        sleep(50);
-        while(Math.abs(leftFrontWheel.getCurrentPosition()) < DISTANCE3) {
-            arcade(0, POWER3, 0, leftFrontWheel, rightFrontWheel, leftBackWheel, rightBackWheel);
+        //TURN
+        while(gyroSensor.getIntegratedZValue() < GYRO2READINGTARGET){
+            arcade(0, 0, POWER2PART2, leftFrontWheel, rightFrontWheel, leftBackWheel, rightBackWheel);
         }
         stopMotors();
 
+        //Forwards
+        resetEncoder(leftFrontWheel);
+        sleep(50);
+        while(Math.abs(leftFrontWheel.getCurrentPosition()) < DISTANCE3) {
+            arcade(POWER3, 0, 0, leftFrontWheel, rightFrontWheel, leftBackWheel, rightBackWheel);
+        }
+        stopMotors();
+
+        //TURN BACK
+        int temp = gyroSensor.getIntegratedZValue()+5;
+        while((gyroSensor.getIntegratedZValue() > GYRO3READINGTARGET) && (gyroSensor.getIntegratedZValue() < temp)) {
+            arcade(0, 0, POWER3PART2, leftFrontWheel, rightFrontWheel, leftBackWheel, rightBackWheel);
+        }
+        stopMotors();
+
+        //strafe to wall
+        sleep(50);
+        while(range.getDistance(DistanceUnit.CM) > CM_FROM_WALL_VALUE) {
+            telemetry.addData("Dist", range.getDistance(DistanceUnit.CM));
+            telemetry.addData("Targ", CM_FROM_WALL_VALUE);
+            telemetry.update();
+            arcade(0, POWER5, 0, leftFrontWheel, rightFrontWheel, leftBackWheel, rightBackWheel);
+        }
+        stopMotors();
+
+/*
         resetEncoder(leftFrontWheel);
         sleep(50);
         while(Math.abs(leftFrontWheel.getCurrentPosition()) < DISTANCE4) {
@@ -149,7 +189,8 @@ public class newAuto extends LinearOpMode {
             arcade(0, POWER5, 0, leftFrontWheel, rightFrontWheel, leftBackWheel, rightBackWheel);
         }
         stopMotors();
-
+*/
+        //backwards to back color sensor
         sleep(50);
         while(colorSensorOnBottom.alpha() < 4) {
             arcade(POWER6, 0, 0, leftFrontWheel, rightFrontWheel, leftBackWheel, rightBackWheel);
@@ -157,22 +198,40 @@ public class newAuto extends LinearOpMode {
         stopMotors();
 
         sleep(100);
+        //press buttons
         handleColor();
         sleep(100);
         rightButtonPusher.setPosition(RIGHT_SERVO_OFF_VALUE);
         leftButtonPusher.setPosition(LEFT_SERVO_OFF_VALUE);
 
+        ///forwards to front color sensor
         while(colorSensorOnBottom.alpha() < COLOR_READING_FOR_LINE) {
             arcade(POWER7, 0, 0, leftFrontWheel, rightFrontWheel, leftBackWheel, rightBackWheel);
         }
         stopMotors();
 
         sleep(100);
+        //press buttons
         handleColor();
         sleep(100);
         rightButtonPusher.setPosition(RIGHT_SERVO_OFF_VALUE);
         leftButtonPusher.setPosition(LEFT_SERVO_OFF_VALUE);
-*/
+
+        //TURN BACK
+        while(gyroSensor.getIntegratedZValue() < GYRO7READINGTARGET){
+            arcade(0, 0, POWER7PART2, leftFrontWheel, rightFrontWheel, leftBackWheel, rightBackWheel);
+        }
+        stopMotors();
+
+        //Back Up
+        resetEncoder(leftFrontWheel);
+        sleep(50);
+        while(Math.abs(leftFrontWheel.getCurrentPosition()) < DISTANCE8) {
+            arcade(POWER8, 0, 0, leftFrontWheel, rightFrontWheel, leftBackWheel, rightBackWheel);
+        }
+        stopMotors();
+
+
     }
 
     public void handleColor() {
@@ -220,5 +279,4 @@ public class newAuto extends LinearOpMode {
         leftBack.setPower(leftBackVal);
         rightBack.setPower(rightBackVal);
     }
-
 }
